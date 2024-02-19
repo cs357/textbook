281da36a6d6ee7f08b5f17ddb2a0fbc5450b76b8
Dev Singh <dsingh14@illinois.edu>
2024-02-19 17:13:24 +0000

    add message

--- _cbtf/create.sh
+++ _cbtf/create.sh
@@ -3,3 +3,4 @@ mkdir -p output/
 rm -rf output/
 httrack https://cs357.github.io/textbook/ -O output/
 echo '<meta HTTP-EQUIV="Refresh" CONTENT="0; URL=cs357.github.io/textbook/index.html">' > output/index.html
+echo "Build done! Upload the concents of the 'output/' directory *without modification* to PrairieLearn to be used in the CBTF."
