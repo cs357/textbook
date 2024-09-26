7b27446288b19edf7e6158690f6cdccaf729b182
Dev Singh <dev@devksingh.com>
2024-09-25 19:19:48 -0500

    upload download script for new domain/settings

--- _cbtf/create.sh
+++ _cbtf/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p output/
 rm -rf output/
-httrack https://cs357.cs.illinois.edu/textbook -O output/ -*textbook/links*
-echo '<meta HTTP-EQUIV="Refresh" CONTENT="0; URL=cs357.github.io/textbook/index.html">' > output/index.html
+httrack https://cs357.cs.illinois.edu/textbook -O output/ --stay-on-same-address -%v -X "https://cs357.cs.illinois.edu/textbook/links/*" -c10
+echo '<meta HTTP-EQUIV="Refresh" CONTENT="0; URL=cs357.cs.illinois.edu/textbook/index.html">' > output/index.html
 echo "Build done! Upload the concents of the 'output/' directory *without modification* to PrairieLearn to be used in the CBTF."
