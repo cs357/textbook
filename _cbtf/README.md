f372a131beddd9ffd3bb089888e1a61668b53477
Dev Singh <dsingh14@illinois.edu>
2025-03-27 21:37:13 +0000

    update deps

--- _cbtf/README.md
+++
@@ -1,4 +0,0 @@
-# Creating CBTF Mirror
-1. launch local server with `make server_noreload`
-2. run `./create-local.sh`
-3. copy the output folder into the PL course.
--- _cbtf/create-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p output/
-rm -rf output/
-wget --mirror --convert-links http://127.0.0.1:4000/textbook/
-mv 127.0.0.1:4000/textbook/ output/
-echo "Build done! Upload the concents of the 'output/' directory *without modification* to PrairieLearn to be used in the CBTF."
